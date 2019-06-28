a1a8ce8557b80e6ccaef4413cfbc178405f2102e
Pavel <bux.booga@gmail.com>
2019-06-28 15:14:42 +0300

    Initial Commit

---
+++ MapOverlayAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapOverlayAnimation
+//
+//  Created by Pavel Lukandiy on 28.06.2019.
+//  Copyright © 2019 Pavel Lukandiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
